6f5672a7af99e467f6bf444140afb731442ccdfe
DELL-DAVID\NOUVEAU <davidlaroche@hotmail.fr>
2018-11-20 11:53:05 +0100

    Ajout fichiers scripts PHP pour alwaysdata

---
+++ PHP/www/scripts/configuration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nathan BERNARD
+ * Date: 05/06/2018
+ * Time: 10:14
+ */
+static $host = "mysql-nathberoux.alwaysdata.net";
+static $database = "nathberoux_genius";
+static $login = "146914_genius_ad";
+static $password = "geniusadmin";
+?>
