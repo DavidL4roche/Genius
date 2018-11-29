84b5f62fe8dceaaff2e2bced124905498c8abf3f
David <davidlaroche@hotmail.fr>
2018-11-29 10:52:19 +0100

    Design page Index - Inscription - Connexion du jeu

---
+++ Assets/scripts/Fonction/DémarrageGenius.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DémarrageGenius : MonoBehaviour {
+
+    void Start()
+    {
+        StartCoroutine(MyCoroutine());
+    }
+
+    IEnumerator MyCoroutine()
+    {
+        yield return new WaitForSeconds(0.02f);
+
+        ChargerLieu charger = new ChargerLieu();
+        charger.Charger("Index2");
+    }
+}
