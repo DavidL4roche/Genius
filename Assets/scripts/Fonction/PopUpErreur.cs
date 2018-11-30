b08b7c36d6e497097faf6758e7c8722ac3b8b100
David <davidlaroche@hotmail.fr>
2018-11-30 14:04:35 +0100

    Création et prise en compte scène "Mot de passe oublié"

---
+++ Assets/scripts/Fonction/PopUpErreur.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpErreur : MonoBehaviour {
+
+    public string typeErreur;
+    public string erreur;
+
+	public void popUpErreur() {
+        
+        switch(typeErreur)
+        {
+            case "Succes":
+                ChargerPopup.Charger("Succes");
+                MessageErreur.messageErreur = erreur;
+                break;
+            default:
+                ChargerPopup.Charger("Erreur");
+                MessageErreur.messageErreur = erreur;
+                break;
+        }
+    }
+}
