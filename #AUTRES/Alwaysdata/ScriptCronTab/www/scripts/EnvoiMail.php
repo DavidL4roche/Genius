f422eab35ca91df2fd75789214078ed389164eff
David <davidlaroche@hotmail.fr>
2019-01-03 11:20:55 +0100

    Envoi de mails après inscription - Contenu simple - Prise en compte HTML

--- #AUTRES/Alwaysdata/ScriptCronTab/www/scripts/EnvoiMail.php
+++ #AUTRES/Alwaysdata/ScriptCronTab/www/scripts/EnvoiMail.php
@@ -4,12 +4,6 @@ require_once "Helper.php";
 
 $helper = new Helper();
 
-/*
-if (mail("davidlaroche@hotmail.fr","My subject","Salut toi")) {
-    print "ok";
-}
-*/
-
 if (isset($_GET["mail"])) {
     $mail = $_GET["mail"];
     $helper->sendMail($mail);
