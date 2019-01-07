da7f36ec96ad87b8d36754e8db9de8cd6e4f70e7
David <davidlaroche@hotmail.fr>
2019-01-07 15:03:13 +0100

    Bouton déconnexion et gestion objets a garder - Bouton déconnexion - Deconnexion redirige vers l'index et supprime les objets mais bug après connexion - Caméra depuis l'index et gardée dans le reste du jeu - Bug Aide résolu

---
+++ Assets/scripts/Fonction/Deconnexion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deconnexion : MonoBehaviour {
+    
+	public void Deconnection () {
+        /*
+        // On détruit tout les objets
+        GameObject[] GameObjects = (FindObjectsOfType<GameObject>() as GameObject[]);
+
+        for (int i = 0; i < GameObjects.Length; i++)
+        {
+            Destroy(GameObjects[i]);
+        }
+        */
+
+        Destroy(GameObject.Find("Joueur(Clone)").gameObject);
+        Destroy(GameObject.Find("Camera").gameObject);
+
+        // On redirige vers l'index
+        ChargerLieu loading = new ChargerLieu();
+        loading.Charger("Index");
+    }
+}
