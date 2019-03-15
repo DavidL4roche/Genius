98219b143e598bab960440368dd80779d57fbe5d
David <davidlaroche@hotmail.fr>
2019-03-15 09:13:21 +0100

    Modification scripts - scriptmission : modification totale, le script tourne correctement et gère les erreurs - configuration : à gérer en fonction de la base (alwaysdata ou serveur) - scriptConnection : script simple testant la connection à une base

--- Assets/scripts/Config/Configuration.cs
+++ Assets/scripts/Config/Configuration.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Configuration : MonoBehaviour {
-    public static string host = "mysql-seriousgameiut.alwaysdata.net";
-    public static string database = "seriousgameiut_bd";
-    public static string login = "171322";
-    public static string password = "azerty12345";
+    public static string host = "http://10.193.15.36/phpmyadmin";
+    public static string database = "serious";
+    public static string login = "seriousadmin";
+    public static string password = "seriousadmin@24";
 
     public static string url = "10.193.15.36/private/";
 }
