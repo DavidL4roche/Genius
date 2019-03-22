cced2594b44f45588c64293ade9f60c96bd9d29a
David <davidlaroche@hotmail.fr>
2019-03-22 10:53:13 +0100

    Création fenêtre information compétence + script correspondant

---
+++ Assets/scripts/Fabrique/FabriqueInfoCompetence.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class FabriqueInfoCompetence : MonoBehaviour {
+    Mission mission = SpawnerMission.LesMissions[VerificationMission.MissionChoisi];
+
+    // Elements graphiques de la fenêtre
+    public Text titreCompetence;
+    public TextMeshPro competence;
+    public Text niveauActuel;
+    public Text niveauRequis;
+
+    public void Start()
+    {
+        // On récupère le champ de description de la compétence
+        competence = GetComponent<TextMeshPro>() ?? gameObject.AddComponent<TextMeshPro>();
+
+        // On attribue les valeurs de la compétence aux champs respectifs
+        titreCompetence.text = "Titre de la compétence";
+        competence.text = "Une petite description des familles.";
+        niveauActuel.text = "99%";
+        niveauRequis.text = "98%";
+    }
+}
