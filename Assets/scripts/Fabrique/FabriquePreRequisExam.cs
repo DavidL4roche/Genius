6fb9a1662dcc0adef9a73e0b9e37897d740040b3
David <davidlaroche@hotmail.fr>
2019-04-19 11:45:36 +0200

    Mise à jour couleur Tuple Compétence - Suppression ValeurBarre dans le Tuple Compétence

--- Assets/scripts/Fabrique/FabriquePreRequisExam.cs
+++ Assets/scripts/Fabrique/FabriquePreRequisExam.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class FabriquePreRequisExam : MonoBehaviour {
+public class FabriquePreRequisExam : MonoBehaviour
+{
     //MissionRessources mission = ListeMissions.listeDeMissions[VerifQuartier.IDQuartier].missions[VerificationMission.MissionChoisi];
     Examen examen = RessourcesBdD.listeDesExamens[VerificationExamen.ExamChoisi];
     public GameObject Tuple;
@@ -11,10 +12,10 @@ public class FabriquePreRequisExam : MonoBehaviour {
     public Text ValeurTupleTexte;
     public Text Divert;
     public Text Social;
-    public Slider ValeurTupleSlider;
-    public Text SliderTexte;
+    public GameObject ValeurBarre;
     public Image ImageTuple;
     public Button lancer;
+    public Image fondLancer;
     GameObject instance;
 
     public void Start()
@@ -25,17 +26,19 @@ public class FabriquePreRequisExam : MonoBehaviour {
     public void blockdesprerequis()
     {
         lancer.interactable = true;
+        ValeurBarre.SetActive(false);
         int decalage = 0;
 
         for (int i = 0; i < examen.CompétencesRequises.Length; ++i)
         {
             Text Texte = nomTuple;
             Texte.text = examen.CompétencesRequises[i].NomCompétence;
-            ValeurTupleSlider.gameObject.SetActive(true);
             ValeurTupleTexte.gameObject.SetActive(false);
             int valeurr = examen.CompétencesRequises[i].Valeur;
+            /*
             ValeurTupleSlider.value = valeurr;
             SliderTexte.text = valeurr.ToString();
+            */
             verificationCompAvecJoueur(examen.CompétencesRequises[i].ID, valeurr);
             instance = Instantiate(Tuple, new Vector3(0F, 0F, 0F), Tuple.transform.rotation);
             instance.transform.parent = GameObject.Find("VerticalLayout1").transform;
@@ -92,13 +95,13 @@ public class FabriquePreRequisExam : MonoBehaviour {
         }
         if (Joueur.MesValeursCompetences[i] >= valeur)
         {
-            ImageTuple.color = new Color(0F, 1F, 0F, 1F);
+            ImageTuple.color = changeColor(true);
         }
         else
         {
-            ImageTuple.color = new Color(1F, 0F, 0F, 1F);
-            Destroy(GameObject.Find("Lancer"));
-            Destroy(GameObject.Find("Ameliorer"));
+            ImageTuple.color = changeColor(false);
+            lancer.interactable = false;
+            fondLancer.color = new Color(255, 255, 255, 125);
         }
     }
     public void verificationRessAvecJoueur(int valeurressource, string nomPrerequis)
@@ -107,16 +110,26 @@ public class FabriquePreRequisExam : MonoBehaviour {
         {
             if (nomPrerequis == RessourcesBdD.listeDesRessources[i].NomRessource)
             {
-                if (Joueur.MesRessources[i] >= valeurressource)
+                if (Joueur.MesRessources[i] < valeurressource)
                 {
-                    ImageTuple.color = new Color(0F, 1F, 0F, 1F);
-                }
-                else
-                {
-                    ImageTuple.color = new Color(1F, 0F, 0F, 1F);
                     lancer.interactable = false;
                 }
             }
+        }
+    }
+
+    // Renvoie la couleur (Color) si la condition est vraie ou fausse
+    public Color changeColor(bool color)
+    {
+        if (color)
+        {
+            Color myColor = new Color32(57, 119, 155, 255);
+            return myColor;
+        }
+        else
+        {
+            Color myColor = new Color32(55, 57, 75, 255);
+            return myColor;
         }
     }
 }
