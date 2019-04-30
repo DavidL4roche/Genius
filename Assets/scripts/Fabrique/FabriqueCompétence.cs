09b0be597b08dc65e89b8e0cc85d9836a8f54cb8
David <davidlaroche@hotmail.fr>
2019-04-30 17:23:39 +0200

    Continuité Profil (en cours) et Compétences (fini)

--- Assets/scripts/Fabrique/FabriqueCompétence.cs
+++ Assets/scripts/Fabrique/FabriqueCompétence.cs
@@ -4,23 +4,37 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class FabriqueCompétence : MonoBehaviour {
-    public GameObject LeTuple;
-    public Slider LeSlider;
-    public Text LeTexteDansLeSlider;
-    public Text TexteDuNom;
-    GameObject instance;
 
+    public GameObject TupleCatégorie;
+    public GameObject TupleCompétences;
+
+    public Text nomCatégorie;
+    public Text nomCompétence;
+    
+    public Text IDExamen;
+    public Text IDCompetence;
+
+    GameObject instance;
 
     private void Start()
     {
-        for(int i = 0; i < RessourcesBdD.listeDesCompétences.Length; ++i)
+        for (int i=0; i<RessourcesBdD.listeDesExamens.Length; ++i)
         {
-            LeSlider.value = Joueur.MesValeursCompetences[i];
-            LeTexteDansLeSlider.text = Joueur.MesValeursCompetences[i].ToString();
-            TexteDuNom.text = RessourcesBdD.listeDesCompétences[i].NomCompétence;
-            instance = Instantiate(LeTuple, new Vector3(0.0F,0.0F,0.0F), LeTuple.transform.rotation);
+            nomCatégorie.text = RessourcesBdD.listeDesExamens[i].NomExamen;
+            instance = Instantiate(TupleCatégorie, new Vector3(0.0F, 0.0F, 0.0F), TupleCatégorie.transform.rotation);
             instance.transform.parent = GameObject.Find("VerticalLayout").transform;
-            instance.transform.name = "Tuple " + (i + 1);
+            instance.transform.name = "TupleCatégorie " + (i + 1);
+
+            for (int j=0; j< RessourcesBdD.listeDesExamens[i].CompétencesRequises.Length; ++j)
+            {
+                nomCompétence.text = RessourcesBdD.listeDesExamens[i].CompétencesRequises[j].NomCompétence;
+                IDExamen.text = (RessourcesBdD.listeDesExamens[i].IDExamen - 1).ToString();
+                IDCompetence.text = RessourcesBdD.listeDesExamens[i].CompétencesRequises[j].ID.ToString();
+
+                instance = Instantiate(TupleCompétences, new Vector3(0.0F, 0.0F, 0.0F), TupleCompétences.transform.rotation);
+                instance.transform.parent = GameObject.Find("VerticalLayout").transform;
+                instance.transform.name = "TupleCompétence " + (i + 1) + "-" + (j + 1);
+            }
         }
     }
 }
---
+++ Assets/scripts/Fonction/VerificationExamenCompetence.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VerificationExamenCompetence : MonoBehaviour {
+
+    public Text IDExamen;
+    public Text IDCompetence;
+
+    public void Cliquer()
+    {
+        VerificationExamen.ExamChoisi = System.Int32.Parse(IDExamen.text);
+        VerificationCompetence.CompetenceChoisie = System.Int32.Parse(IDCompetence.text);
+    }
+}
