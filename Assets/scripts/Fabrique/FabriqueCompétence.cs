e30971841e7cc6b8eb4e84201d3b2a166d65926c
David <davidlaroche@hotmail.fr>
2019-05-02 09:09:12 +0200

    Différenciation compétence acquises - Bouton Trophées inactif dans Profil

--- Assets/scripts/Fabrique/FabriqueCompétence.cs
+++ Assets/scripts/Fabrique/FabriqueCompétence.cs
@@ -6,13 +6,13 @@ using UnityEngine.UI;
 public class FabriqueCompétence : MonoBehaviour {
 
     public GameObject TupleCatégorie;
-    public GameObject TupleCompétences;
-
     public Text nomCatégorie;
-    public Text nomCompétence;
-    
     public Text IDExamen;
+
+    public GameObject TupleCompétences;
+    public Text nomCompétence;
     public Text IDCompetence;
+    public RawImage colorTupleComp;
 
     GameObject instance;
 
@@ -30,6 +30,34 @@ public class FabriqueCompétence : MonoBehaviour {
                 nomCompétence.text = RessourcesBdD.listeDesExamens[i].CompétencesRequises[j].NomCompétence;
                 IDExamen.text = (RessourcesBdD.listeDesExamens[i].IDExamen - 1).ToString();
                 IDCompetence.text = RessourcesBdD.listeDesExamens[i].CompétencesRequises[j].ID.ToString();
+
+                // On compare la valeur demandée de la compétence par l'examen avec la valeur du joueur
+                int valComp = 0;
+                for (; valComp < RessourcesBdD.listeDesExamens[i].CompétencesRequises.Length; ++valComp)
+                {
+                    if (RessourcesBdD.listeDesExamens[i].CompétencesRequises[valComp].ID == RessourcesBdD.listeDesExamens[i].CompétencesRequises[j].ID)
+                    {
+                        break;
+                    }
+                }
+
+                int valJoueur = 0;
+                for (; valJoueur < RessourcesBdD.listeDesCompétences.Length; ++valJoueur)
+                {
+                    if (RessourcesBdD.listeDesExamens[i].CompétencesRequises[j].ID == RessourcesBdD.listeDesCompétences[valJoueur].ID)
+                    {
+                        break;
+                    }
+                }
+
+                if (RessourcesBdD.listeDesExamens[i].CompétencesRequises[valComp].Valeur < Joueur.MesValeursCompetences[valJoueur])
+                {
+                    colorTupleComp.color = new Color32(43, 152, 26, 255);
+                }
+                else
+                {
+                    colorTupleComp.color = new Color32(49, 97, 125, 255);
+                }
 
                 instance = Instantiate(TupleCompétences, new Vector3(0.0F, 0.0F, 0.0F), TupleCompétences.transform.rotation);
                 instance.transform.parent = GameObject.Find("VerticalLayout").transform;
