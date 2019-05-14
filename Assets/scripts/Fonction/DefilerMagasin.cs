6bc239f904b08201ad5bf75edd5e515d55cc0656
David <davidlaroche@hotmail.fr>
2019-05-14 09:38:58 +0200

    Système de défilement + fenêtre confirmation achat

--- Assets/scripts/Fonction/DefilerMagasin.cs
+++ Assets/scripts/Fonction/DefilerMagasin.cs
@@ -9,11 +9,22 @@ public class DefilerMagasin : MonoBehaviour {
 
 	public void Precedent()
     {
-        Debug.Log("Précédent");
+        HorizontalLayout.transform.position = new Vector3(HorizontalLayout.transform.position.x + 352, HorizontalLayout.transform.position.y, HorizontalLayout.transform.position.z);
     }
 
     public void Suivant()
     {
-        Debug.Log("Suivant");
+        HorizontalLayout.transform.position = new Vector3(HorizontalLayout.transform.position.x - 352, HorizontalLayout.transform.position.y, HorizontalLayout.transform.position.z);
+    }
+
+    public float getSuivantMultiple(float x)
+    {
+        float multiple = 1056.35F; // Taille de l'Horizontal Bar
+        while (x > multiple)
+        {
+            multiple -= 352;
+        }
+
+        return multiple;
     }
 }
