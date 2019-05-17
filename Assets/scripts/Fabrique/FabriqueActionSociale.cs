f6a9ab8495eba3499268eb65f5d21d101a9bb6fa
David <davidlaroche@hotmail.fr>
2019-05-17 11:21:16 +0200

    Mise en place Resultat Action Sociale

--- Assets/scripts/Fabrique/FabriqueActionSociale.cs
+++ Assets/scripts/Fabrique/FabriqueActionSociale.cs
@@ -8,9 +8,11 @@ public class FabriqueActionSociale : MonoBehaviour {
     string choix = VerifActionSocial.ActionSocialeChoisie;
 
     public Text LeNomDuJoueur;
+    public Text Question;
 
     public GameObject Bouton;
     public GameObject tuple;
+    public RawImage couleurTuple;
     public RawImage CadreBlanc;
     public Text LeTexteSlider;
     public RawImage imageAction;
@@ -27,6 +29,7 @@ public class FabriqueActionSociale : MonoBehaviour {
         LeNomDuJoueur.text = joueur.SonNom;
         if (choix == "Objet")
         {
+            Question.text = "Quel objet souhaitez-vous essayer d'obtenir ?";
             //QuantitéObjet.gameObject.SetActive(true);
             for (int i = 0; i<RessourcesBdD.listeDesObjets.Length; ++i)
             {
@@ -44,9 +47,11 @@ public class FabriqueActionSociale : MonoBehaviour {
         }
         else
         {
+            Question.text = "Quel compétence souhaitez-vous améliorer ?";
             for (int i = 0; i < RessourcesBdD.listeDesCompétences.Length; ++i)
             {
-                if (joueur.SesCompétences[i] == 100)
+                couleurTuple.color = new Color32(6, 212, 168, 255);
+                if (joueur.SesCompétences[i] > 75)
                 {
                     TexteTuple.text = ">_" + RessourcesBdD.listeDesCompétences[i].NomCompétence;
                     // LeSlider.GetComponent<Slider>().value = joueur.SesCompétences[i];
@@ -58,6 +63,22 @@ public class FabriqueActionSociale : MonoBehaviour {
                     instance.transform.parent = GameObject.Find("VerticalLayout").transform;
                 }
             }
+            for (int i = 0; i < RessourcesBdD.listeDesCompétences.Length; ++i)
+            {
+                couleurTuple.color = new Color32(49, 97, 125, 255);
+                if (joueur.SesCompétences[i] <= 75)
+                {
+                    TexteTuple.text = ">_" + RessourcesBdD.listeDesCompétences[i].NomCompétence;
+                    // LeSlider.GetComponent<Slider>().value = joueur.SesCompétences[i];
+                    LeTexteSlider.text = joueur.SesCompétences[i].ToString();
+                    imageAction.texture = Resources.Load<Texture>("icones/Icon_diplome");
+                    Bouton.transform.name = i.ToString();
+                    instance = Instantiate(tuple, new Vector3(0, 0, 0), tuple.transform.rotation);
+                    instance.transform.name = "Compétence " + i;
+                    instance.transform.parent = GameObject.Find("VerticalLayout").transform;
+                }
+            }
+            //couleurTuple.color = new Color32(49, 97, 125, 255);
         }
         //QuantitéObjet.gameObject.SetActive(true);
         Bouton.gameObject.SetActive(false);
--- Assets/scripts/Fabrique/FabriqueResultatSocial.cs
+++ Assets/scripts/Fabrique/FabriqueResultatSocial.cs
@@ -7,50 +7,69 @@ public class FabriqueResultatSocial : MonoBehaviour {
     public GameObject Tuple;
     public Text nomTuple;
     public Text Quantité;
-    public Image Cadre;
+    public RawImage Cadre;
     GameObject instance;
     public void Start()
     {
-        Cadre.color = new Color(0F, 1F, 0F, 1F);
+        Cadre.color = new Color32(6, 212, 168, 255);
         switch (VerifActionSocial.ActionSocialeChoisie)
         {
             case "Objet":
+                // On génère l'objet gagné
                 Objet obj = gainObjetSocial();
+
+                // On fait gagner +10 de Social au joueur
                 int Social = 10;
-                // les tuples
+
+                // Tuple Social
                 nomTuple.text = "Social";
-                Quantité.text = "+"+Social.ToString();
+                Quantité.text = "+" + Social.ToString();
                 instance = Instantiate(Tuple, new Vector3(0, 0, 0), Tuple.transform.rotation);
                 instance.transform.name = "Social";
                 instance.transform.parent = GameObject.Find("VerticalLayout").transform;
+
+                // Tuple Objet
                 nomTuple.text = obj.Nom;
                 Quantité.text = "1";
                 instance = Instantiate(Tuple, new Vector3(0, 0, 0), Tuple.transform.rotation);
                 instance.transform.name = "Objet";
                 instance.transform.parent = GameObject.Find("VerticalLayout").transform;
+
+                // On ajoute les gains en Social et en Objet au Joueur
                 GainSocial(Social);
                 GainObjet(obj);
                 break;
+
             case "Compétence":
+                //On génère les gains de compétences et de social
                 int[] gaincompetsocial = gainCompSocial();
+
+                // Tuple Social
                 nomTuple.text = "Social";
                 Quantité.text = gaincompetsocial[1].ToString();
                 instance = Instantiate(Tuple, new Vector3(0, 0, 0), Tuple.transform.rotation);
                 instance.transform.name = "Social";
                 instance.transform.parent = GameObject.Find("VerticalLayout").transform;
+
+                // Tuple Compétence
                 nomTuple.text = "Compétence";
                 Quantité.text = "+"+ gaincompetsocial[0].ToString();
                 instance = Instantiate(Tuple, new Vector3(0, 0, 0), Tuple.transform.rotation);
                 instance.transform.name = "Compétence";
                 instance.transform.parent = GameObject.Find("VerticalLayout").transform;
+
+                // On ajoute les gains en Social et en Compétence au Joueur
                 GainSocial(gaincompetsocial[1]);
                 GainComp(gaincompetsocial[0], RessourcesBdD.listeDesCompétences[VerifTupleActionSocial.TupleChoisieActionSocial].ID);
                 break;
+
             default:
                 break;
         }
         
     }
+
+    // Retourne l'objet de l'ami cliqué (10% de chances) ou un objet de rang C (90% de chances)
     Objet gainObjetSocial()
     {
         Objet obj;
@@ -72,6 +91,8 @@ public class FabriqueResultatSocial : MonoBehaviour {
         }
         return obj;
     }
+
+    // Retourne les valeurs de compétences gagnées
     int[] gainCompSocial()
     {
         int[] les2Valeurs= new int[2];
@@ -80,41 +101,51 @@ public class FabriqueResultatSocial : MonoBehaviour {
         int valeurcompami = Joueur.MesAmis[VerificationAmi.AmiChoisi].SesCompétences[VerifTupleActionSocial.TupleChoisieActionSocial];
         if (valeurcompami > 75)
         {
+            Debug.Log("Valeur compétence ami > 75 !");
             int random = Random.Range(0, 3);
             les2Valeurs[0] = tabDeValeursGainComp[random];
         }
         else if (valeurcompami > 50)
         {
+            Debug.Log("Valeur compétence ami > 50 !");
             int random = Random.Range(0,2);
             les2Valeurs[0] = tabDeValeursGainComp[random];
         }
         else if (valeurcompami > 25)
         {
+            Debug.Log("Valeur compétence ami > 25 !");
             int random = Random.Range(0, 1);
             les2Valeurs[0] = tabDeValeursGainComp[random];
         }
         else
         {
+            Debug.Log("Valeur compétence ami nulle !");
             les2Valeurs[0] = tabDeValeursGainComp[0]; 
         }
         switch (les2Valeurs[0])
         {
-            case 40:
+            case 10://40:
+                Debug.Log("Gain comp : 10");
                 les2Valeurs[1] = tabDeValeurSocial[3];
                 break;
-            case 20:
+            case 5://20:
+                Debug.Log("Gain comp : 5");
                 les2Valeurs[1] = tabDeValeurSocial[2];
                 break;
-            case 10:
+            case 2:// 10:
+                Debug.Log("Gain comp : 2");
                 les2Valeurs[1] = tabDeValeurSocial[1];
                 break;
             default:
+                Debug.Log("Gain comp : 1");
                 les2Valeurs[1] = tabDeValeurSocial[0];
                 break;
             
         }
         return les2Valeurs;
     }
+
+    // On ajoute l'objet en paramètres dans les objets du joueur
     void GainObjet(Objet obj)
     {
         for(int i = 0; i < RessourcesBdD.listeDesObjets.Length; ++i)
@@ -125,6 +156,8 @@ public class FabriqueResultatSocial : MonoBehaviour {
             }
         }
     }
+
+    // On ajoute la valeur d'une compétence (paramètre) à la compétence du joueur
     void GainComp(int valcomp, int idcomp)
     {
         for (int i = 0;i<RessourcesBdD.listeDesCompétences.Length;++i )
@@ -143,6 +176,8 @@ public class FabriqueResultatSocial : MonoBehaviour {
             }
         }
     }
+
+    // On ajoute n (paramètre) points de Social au joueur
     void GainSocial(int val)
     {
         for (int i = 0; i<RessourcesBdD.listeDesRessources.Length;++i)
