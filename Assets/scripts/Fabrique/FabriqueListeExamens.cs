f08d373711fbd3c683e3fcdf652e97ef6322d3c2
David <davidlaroche@hotmail.fr>
2019-05-29 16:18:37 +0200

    Affichage amélioré Examens - Faisabilité ou non - Tri en fonction de Disponibles/Indisponibles/Acquis - Intégration graphismes

--- Assets/scripts/Fabrique/FabriqueListeExamens.cs
+++ Assets/scripts/Fabrique/FabriqueListeExamens.cs
@@ -6,9 +6,14 @@ using UnityEngine.UI;
 public class FabriqueListeExamens : MonoBehaviour {
     
     public GameObject Tuple;
-    public RawImage imageTuple;
+    public Image imageTuple;
     public Text nomExamen;
+    public RawImage iconeCheck;
     public Button examenIDGO;
+
+    public GameObject TupleCategorie;
+    public Text NomCategorie;
+
     GameObject instance;
 
     // Use this for initialization
@@ -19,14 +24,21 @@ public class FabriqueListeExamens : MonoBehaviour {
 	// Update is called once per frame
 	void genererListeExamens () {
 
-        // On génère chaque tuple d'examen
+        // On instancie la catégorie "Disponibles"
+        NomCategorie.text = "Disponibles";
+        instance = Instantiate(TupleCategorie, new Vector3(0F, 0F, 0F), Tuple.transform.rotation);
+        instance.transform.parent = GameObject.Find("ListeExamens").transform;
+        instance.transform.name = "TupleCategorieDisponibles";
+
+        // On génère chaque tuple d'examen disponibles
         for (int i=0; i<RessourcesBdD.listeDesExamens.Length; ++i)
         {
             // On rend interactible le bouton (ZoneInfo)
             examenIDGO.interactable = true;
 
             // On réinitialise la couleur du Tuple
-            imageTuple.color = new Color32(49, 97, 125, 255);
+            imageTuple.color = new Color32(51, 54, 82, 255);
+            iconeCheck.enabled = false;
 
             // Définition nom et id examen
             Examen examen = RessourcesBdD.listeDesExamens[i];
@@ -34,6 +46,106 @@ public class FabriqueListeExamens : MonoBehaviour {
             Texte.text = truncateString(examen.NomExamen, 15);
             Text examenID = examenIDGO.GetComponentInChildren<Text>();
             examenID.text = (examen.IDExamen-1).ToString();
+
+            bool playing = true;
+            // On vérifie si le joueur peut passer l'examen
+            for (int j = 0; j < examen.CompétencesRequises.Length; ++j)
+            {
+                int valeurr = examen.CompétencesRequises[j].Valeur;
+                if (!verificationCompAvecJoueur(examen.CompétencesRequises[j].ID, valeurr))
+                {
+                    playing = false;
+                    break;
+                }
+            }
+
+            // On vérifie si l'examen a déjà été passé par le joueur
+            RessourcesBdD.recupExamensNonJouables();
+            foreach (int id in RessourcesBdD.listeDesExamensNonJouables)
+            {
+                if (id == examen.IDExamen)
+                {
+                    playing = false;
+                }
+            }
+
+            if (playing)
+            {
+                imageTuple.color = new Color32(49, 97, 125, 255);
+
+                // On instancie le tuple
+                instance = Instantiate(Tuple, new Vector3(0F, 0F, 0F), Tuple.transform.rotation);
+                instance.transform.parent = GameObject.Find("ListeExamens").transform;
+                instance.transform.name = "TupleExamen" + (i + 1);
+            }
+        }
+
+        // On instancie la catégorie "Indisponibles"
+        NomCategorie.text = "Indisponibles";
+        instance = Instantiate(TupleCategorie, new Vector3(0F, 0F, 0F), Tuple.transform.rotation);
+        instance.transform.parent = GameObject.Find("ListeExamens").transform;
+        instance.transform.name = "TupleCategorieIndisponibles";
+
+        // On génère chaque tuple d'examen indisponibles
+        for (int i = 0; i < RessourcesBdD.listeDesExamens.Length; ++i)
+        {
+            // On rend interactible le bouton (ZoneInfo)
+            examenIDGO.interactable = true;
+
+            // On réinitialise la couleur du Tuple
+            imageTuple.color = new Color32(51, 54, 82, 255);
+            iconeCheck.enabled = false;
+
+            // Définition nom et id examen
+            Examen examen = RessourcesBdD.listeDesExamens[i];
+            Text Texte = nomExamen;
+            Texte.text = truncateString(examen.NomExamen, 15);
+            Text examenID = examenIDGO.GetComponentInChildren<Text>();
+            examenID.text = (examen.IDExamen - 1).ToString();
+
+            bool playing = true;
+            // On vérifie si le joueur peut passer l'examen
+            for (int j = 0; j < examen.CompétencesRequises.Length; ++j)
+            {
+                int valeurr = examen.CompétencesRequises[j].Valeur;
+                if (!verificationCompAvecJoueur(examen.CompétencesRequises[j].ID, valeurr))
+                {
+                    playing = false;
+                    break;
+                }
+            }
+
+            if (!playing)
+            {
+                // On instancie le tuple
+                instance = Instantiate(Tuple, new Vector3(0F, 0F, 0F), Tuple.transform.rotation);
+                instance.transform.parent = GameObject.Find("ListeExamens").transform;
+                instance.transform.name = "TupleExamen" + (i + 1);
+            }
+        }
+
+        // On instancie la catégorie "Acquis"
+        NomCategorie.text = "Acquis";
+        instance = Instantiate(TupleCategorie, new Vector3(0F, 0F, 0F), Tuple.transform.rotation);
+        instance.transform.parent = GameObject.Find("ListeExamens").transform;
+        instance.transform.name = "TupleCategorieAcquis";
+
+        // On génère chaque tuple d'examen acquis
+        for (int i = 0; i < RessourcesBdD.listeDesExamens.Length; ++i)
+        {
+            // On rend interactible le bouton (ZoneInfo)
+            examenIDGO.interactable = true;
+
+            // On réinitialise la couleur du Tuple
+            imageTuple.color = new Color32(51, 54, 82, 255);
+            iconeCheck.enabled = false;
+
+            // Définition nom et id examen
+            Examen examen = RessourcesBdD.listeDesExamens[i];
+            Text Texte = nomExamen;
+            Texte.text = truncateString(examen.NomExamen, 15);
+            Text examenID = examenIDGO.GetComponentInChildren<Text>();
+            examenID.text = (examen.IDExamen - 1).ToString();
 
             // On vérifie si l'examen a déjà été passé par le joueur
             RessourcesBdD.recupExamensNonJouables();
@@ -43,13 +155,14 @@ public class FabriqueListeExamens : MonoBehaviour {
                 {
                     examenIDGO.interactable = false;
                     imageTuple.color = new Color32(6, 212, 168, 255);
+                    iconeCheck.enabled = true;
+
+                    // On instancie le tuple
+                    instance = Instantiate(Tuple, new Vector3(0F, 0F, 0F), Tuple.transform.rotation);
+                    instance.transform.parent = GameObject.Find("ListeExamens").transform;
+                    instance.transform.name = "TupleExamen" + (i + 1);
                 }
             }
-
-            // On instancie le tuple
-            instance = Instantiate(Tuple, new Vector3(0F, 0F, 0F), Tuple.transform.rotation);
-            instance.transform.parent = GameObject.Find("ListeExamens").transform;
-            instance.transform.name = "TupleExamen" + (i + 1);
         }
     }
 
@@ -63,6 +176,26 @@ public class FabriqueListeExamens : MonoBehaviour {
         else
         {
             return myStr;
+        }
+    }
+
+    bool verificationCompAvecJoueur(int idComp, int valeur)
+    {
+        int i = 0;
+        for (; i < RessourcesBdD.listeDesCompétences.Length; ++i)
+        {
+            if (idComp == RessourcesBdD.listeDesCompétences[i].ID)
+            {
+                break;
+            }
+        }
+        if (Joueur.MesValeursCompetences[i] >= valeur)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 }
