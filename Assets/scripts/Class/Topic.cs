9a14f8c389cb9d9e05437624cf17396b1f04e4fc
David <davidlaroche@hotmail.fr>
2019-06-05 08:30:21 +0200

    Ajout Menu Aide

---
+++ Assets/scripts/Class/Topic.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Topic : MonoBehaviour {
+
+    public int IDTopic;
+    public string TitreTopic;
+    public string CorpsTopic;
+
+    public Topic(int id, string titre, string corps)
+    {
+        IDTopic = id;
+        TitreTopic = titre;
+        CorpsTopic = corps;
+    }
+}
