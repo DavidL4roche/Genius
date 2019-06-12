98c2a96fd2ade987f14b40e2633f58cf731123fa
David <davidlaroche@hotmail.fr>
2019-06-12 17:26:22 +0200

    Mise en places Ecrans pour Tutoriel

---
+++ Assets/scripts/Fonction/DecalerCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecalerCamera : MonoBehaviour {
+
+    public Camera cameraJeu;
+    public float valueX = 0;
+    public float valueY = 0;
+
+	public void ChangerPositionCamera() {
+        cameraJeu.transform.position = new Vector3(valueX, valueY, -380);
+	}
+}
