b8254fe10661c67096c3a7ec2b2c982bea9953e5
David <davidlaroche@hotmail.fr>
2019-06-21 16:12:33 +0200

    Mise à jour Genius

--- #AUTRES/Website/index.php
+++ #AUTRES/Website/index.php
@@ -82,7 +82,7 @@
                        class="list-group-item list-group-item-action">
                         <img class="linkLogo mr-2" src="https://image.flaticon.com/icons/svg/25/25231.svg">GitHub
                     </a>
-                    <a target="_blank" href="https://appetize.io/app/11m5x03cf6wnneu691krd2cyzr"
+                    <a target="_blank" href="https://appetize.io/app/z3d5pm1yukrtqe8ruukjajgw7w"
                        class="list-group-item list-group-item-action">
                         <img class="linkLogo mr-2" src="https://appetize.io/images/logo1_colored_tight.png">Appetize.io
                     </a>
