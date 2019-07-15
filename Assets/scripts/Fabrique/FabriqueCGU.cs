82f661318208024ad08ed7f39de54518e223a16a
David <davidlaroche@hotmail.fr>
2019-07-15 11:28:45 +0200

    Rédaction et intégration CGU Genius

---
+++ Assets/scripts/Fabrique/FabriqueCGU.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FabriqueCGU : MonoBehaviour {
+
+    public Text zoneTexte;
+
+    private void Start()
+    {
+        try
+        {
+            TextReader reader;
+            string fileName = "#AUTRES/CGU.txt";
+            reader = new StreamReader(fileName);
+            string result = reader.ReadToEnd();
+            Debug.Log(result);
+            zoneTexte.text = result;
+            reader.Close();
+        }
+
+        catch (IOException e)
+        {
+            Debug.Log("Le fichier ne peut être lu :");
+            Debug.Log(e.Message);
+        }
+    }
+}
